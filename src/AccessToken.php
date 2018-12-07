55256d672afd707de8b0acece038a209ccc80d4f
waynesun01 <waynesun01@gmail.com>
2018-12-07 16:06:18 +0800

    Add files via upload

---
+++ src/AccessToken.php
@@ -0,0 +1,38 @@
+<?php
+namespace waynesun01\weixin;
+use Yii;
+
+use weixin;
+class AccessToken extends weixin{
+    const API_TOKEN='https://api.weixin.qq.com/cgi-bin/token';
+
+
+    public function accessToken($refresh=false){
+        $key="wx-accesstoken-{$this->conf['app_id']}";
+        if($refresh){
+            Yii::$app->cache->delete($key);
+        }
+        $accessToken=Yii::$app->cache->get($key);
+        if(!$accessToken){
+            $getData=$this->getAccessToken();
+            $accessToken=$getData['access_token'];
+            Yii::$app->cache->set($key,$accessToken,($getData['express_in']-60));
+        }
+        return $accessToken;
+
+    }
+
+    private function getAccessToken(){
+        $params=[
+            'grant_type'=>'client_credential',
+            'appid'=>'',
+            'secret'=>''
+        ];
+        $respoense=$this->get(self::API_TOKEN,$params)->send();
+        if($respoense->isOk){
+            
+        }else{
+            
+        }
+    }
+}
