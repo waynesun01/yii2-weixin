ba0864981dee2a92ae3a411e8a4ea1b35595fc4a
waynesun01 <waynesun01@gmail.com>
2018-12-07 16:06:49 +0800

    Update weixin.php

--- src/weixin.php
+++ src/weixin.php
@@ -1,3 +1,24 @@
-<?php
+<?php 
+namespace waynesun01\weixin;
+use yii\base\Components;
+use yii\httpClient\Client;
+class weixin extends Component
+{
+    private $httpClient;
+    private $conf;
 
-?>
+    public function init(){
+        parent::init();
+        $this->httpClient=new Client();
+        $this->conf=[
+            'appid'=>'111'
+        ];
+    }
+
+    protected function get($url,$params=[],$header=[],$options=[]){
+        return $this->httpClient->get($url,$params,$header,$options);
+    }
+    protected function post($url,$params=[],$header=[],$options=[]){
+        return $this->httpClient->post($url,$params,$header,$options);
+    }
+}
